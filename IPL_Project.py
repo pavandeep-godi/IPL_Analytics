2d19de1b9698fe85e3f546c8f4583203c03c205f
pavandeep-godi <pavandeep_459@gmail.com>
2026-08-13 23:20:47 +0530

    removed type casting all together

--- IPL_Project.py
+++ IPL_Project.py
@@ -66,19 +66,6 @@ st.markdown(f"""
 def load_ball_data(file_path: str) -> pd.DataFrame:
     df = pd.read_csv(file_path)
 
-    # Downcast string columns to 'category' to drastically reduce memory usage
-    category_cols = ['venue', 'batting_team', 'bowling_team', 'batter', 'bowler', 'wicket_kind']
-
-    for col in category_cols:
-        if col in df.columns:
-            df[col] = df[col].astype('category')
-
-    # Keep numeric columns intact:
-    numeric_cols = ['runs_batter', 'runs_extras', 'runs_total', 'runs_bowler', 'valid_ball', 'bowler_wicket']
-    for col in numeric_cols:
-        if col in df.columns:
-            df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0)
-
     return df
 
 # Load base dataset (Loaded ONCE across all user interactions)
