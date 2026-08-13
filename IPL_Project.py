949b5a693d9acb213c52cbad3b8087c3216db8f1
pavandeep-godi <pavandeep_459@gmail.com>
2026-08-13 09:32:34 +0530

    title change

--- IPL_Project.py
+++ IPL_Project.py
@@ -33,9 +33,7 @@ st.title("IPL Analytics")
 image = Image.open("IPL_LOGO.jpg")
 st.image(image)
 
-st.title(
-    "Welcome to your all-in-one, single-page solution for deep-dive IPL auction analytics, player performance breakdowns, and strategic insights. Whether you are analyzing all-round impact or evaluating fielding metrics, this interactive platform delivers everything you need at a glance."
-)
+st.title("Welcome to your all-in-one, single-page solution for deep-dive IPL auction analytics, player performance breakdowns, and strategic insights. Whether you are analyzing all-round impact or evaluating fielding metrics, this interactive platform delivers everything you need at a glance.")
 
 
 ball2ball_data=pd.read_csv("ball2ball_df.csv")
